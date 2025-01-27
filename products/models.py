6160e87ca805667f72092ad4f8d0c21106689a78
Keven9801 <skeven98@gmail.com>
2025-01-27 19:37:21 -0300

    adicionado campo localização para organizar os produtos nas prateleiras

--- products/models.py
+++ products/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True)
     serie_number = models.CharField(max_length=200, null=True, blank=True)
     cost_price = models.DecimalField(max_digits=20, decimal_places=2)
+    location = models.CharField(max_length=20, null=True, blank=True)
     selling_price = models.DecimalField(max_digits=20, decimal_places=2)
     quantity = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
